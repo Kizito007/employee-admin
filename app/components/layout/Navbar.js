3b4b8a5cde23e0ef2bf2c4129b2d5def42fe3da0
Kizito007 <nwakakelechi49gmail.com>
2024-12-16 15:46:37 +0100

    fix: nav

--- app/components/layout/Navbar.js
+++ app/components/layout/Navbar.js
@@ -1,11 +1,8 @@
 "use client"
-import { useEffect, useState } from "react";
 import Image from "next/image";
-import axios from "axios";
 import {
   Disclosure,
   DisclosureButton,
-  DisclosurePanel,
 } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
@@ -16,7 +13,6 @@ import Patryx from "@/app/components/images/whiteP.svg"
 const navigation = [
   { name: "Dashboard", href: "/dashboard", current: false },
   { name: "Departments", href: "/departments", current: false },
-  // { name: "Statistics", href: "/statistics", current: false },
 ];
 
 function classNames(...classes) {
@@ -24,34 +20,10 @@ function classNames(...classes) {
 }
 
 export default function Navbar() {
-  const [admin, setAdmin] = useState({});
   const router = useRouter();
-
-  useEffect(() => {
-    const fetchAdmin = async () => {
-      try {
-        const jwt = localStorage.getItem('jwt')
-        const adminId = localStorage.getItem('adminId')
-        const response = await axios.get(
-          `https://school-project-backend-p17b.onrender.com/api/v1/commerce/admin/admin-mgmt/admins/${adminId}`,
-          {
-            headers: {
-              'Authorization': `Bearer ${jwt}`
-            }
-          }
-        );
-        setAdmin(response.data.data);
-      } catch (err) {
-        console.log(err)
-      }
-    };
-
-    fetchAdmin();
-  }, []);
 
   const signOut = () => {
     localStorage.removeItem('jwt');
-    localStorage.removeItem('faceMatch');
     router.push('/login');
   };
   
@@ -113,46 +85,6 @@ export default function Navbar() {
               </div>
             </div>
           </div>
-
-          <DisclosurePanel className="md:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-              {navigation.map((item) => (
-                <DisclosureButton
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  aria-current={item.current ? "page" : undefined}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                >
-                  {item.name}
-                </DisclosureButton>
-              ))}
-            </div>
-            <div className="border-t border-gray-700 pb-3 pt-4">
-              <div className="flex items-center px-5">
-                <div className="shrink-0">
-                  <img
-                    alt=""
-                    src={admin?.photo?.url}
-                    className="size-10 rounded-full"
-                  />
-                </div>
-                <div className="ml-3">
-                  <div className="text-base/5 font-medium text-white">
-                    {admin?.username}
-                  </div>
-                  <div className="text-sm font-medium text-gray-400">
-                    {admin.email}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </DisclosurePanel>
         </Disclosure>
       </div>
     </>
